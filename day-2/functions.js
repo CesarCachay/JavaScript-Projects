46229d57b677d204a9fe1a731e5a91dc10444b86
Cesar Cachay <cesarcachay1996@gmail.com>
2019-06-30 20:50:52 -0500

    add new methods

--- day-2/functions.js
+++ day-2/functions.js
@@ -1,60 +1,74 @@
 // Function "POP"
 
-const pop = function (arr) {
-  const lastItem = arr[arr.length - 1]
-  arr.length = arr.length - 1
-  return lastItem
-}
+const pop = function(arr) {
+  const lastItem = arr[arr.length - 1];
+  arr.length = arr.length - 1;
+  return lastItem;
+};
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(pop(arr));
+const arrPop = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(pop(arrPop));
 
 // Function "EVERY"
 
-const arr = [1, 3, 5, 7, 9, 11, 23];
+const arrEvery = [1, 3, 5, 7, 9, 11, 23];
 
 function isBiggerThanTwenty(num) {
-  return num > 20
+  return num > 20;
 }
 
-const every = function (arr, fn) {
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i]) == false) {
-      return false
+const every = function(arrEvery, fn) {
+  for (let i = 0; i < arrEvery.length; i++) {
+    if (!fn(arrEvery[i])) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
-every(arr, isBiggrThanTwenty)
+every(arrEvery, isBiggrThanTwenty);
 console.log(every(every));
 
 // Function "SOME"
 
-const arr = [3, 6, 8, -2, 10];
+const arrSome = [3, 6, 8, -2, 10];
 
 function isNegative(number) {
-  return number < 0
+  return number < 0;
 }
 
-const some = function (arr, fn) {
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i]) == true) {
-      return true
+const some = function(arrSome, fn) {
+  for (let i = 0; i < arrSome.length; i++) {
+    if (fn(arrSome[i])) {
+      return true;
     }
   }
-  return false
-}
+  return false;
+};
 
-some(arr, isNegative)
+some(arrSome, isNegative);
 
 // Function "FIND"
+const find = (arr, fn) => {
+  for (let index = 0; index < arr.length; index++) {
+    if (fn(arr[index])) return arr[index];
+  }
+  return undefined;
+};
 
-
-
+const arrayFind = [5, 12, 8, 130, 44];
+find(arrayFind, num => num > 10);
 
 // Function "CONCAT"
 
+const concat = (arr, ...elements) => {
+  for (let index = 0; index < elements.length; index++) {
+    arr.push(elements[index]);
+  }
+  return arr;
+};
 
+const arrConcat = [1, 2, 3, 4, 5];
+concat(arrConcat, 6, 7);
 
 // Function "JOIN"
